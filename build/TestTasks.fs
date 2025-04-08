7c263d600b1eed853f072c1c5bd60c6c1c28cdc8
Kevin Schneider <schneider.kev@outlook.de>
2025-04-08 11:30:06 +0200

    Collect test coverage

--- build/TestTasks.fs
+++ build/TestTasks.fs
@@ -13,6 +13,7 @@ let runTests = BuildTask.create "RunTests" [clean; buildSolution] {
         testProject
         |> Fake.DotNet.DotNet.test (fun testParams ->
             { testParams with
+                Collect = Some "XPlat Code Coverage"
                 Logger = Some "console;verbosity=detailed"
                 Configuration = DotNet.BuildConfiguration.fromString configuration
                 NoBuild = true
